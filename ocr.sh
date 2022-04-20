d720150e1d6f082a8a53a093b0476a250d9da745
Thura Aung <thuraaung.it.2017@kyauksetu.edu.mm>
2022-04-20 14:15:40 +0630

    Update ocr.sh

--- ocr.sh
+++ ocr.sh
@@ -2,7 +2,7 @@
 # OCR with freely available Google Tesseract OCR Engine (perparing the baseline)
 # Author : Thura Aung
 # Note: Install tesseract OCR engine on your computer first
-# How to run: tesseract-ocr.sh <language> <sample-images-path> <outputpath>
+# How to run: ocr.sh <language> <sample-images-path> <outputpath>
 
 LANG=$1;
 PATH=$2;
