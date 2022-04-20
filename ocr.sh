b5d603548d639f17b2f6013110e6df5c696a4d5a
Thura Aung <thuraaung.it.2017@kyauksetu.edu.mm>
2022-04-20 14:12:58 +0630

    Create ocr.sh

---
+++ ocr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# OCR with freely available Google Tesseract OCR Engine (perparing the baseline)
+# Author : Thura Aung
+# Note: Install tesseract OCR engine on your computer first
+# How to run: tesseract-ocr.sh <language> <sample-images-path> <outputpath>
+
+LANG=$1;
+PATH=$2;
+OUTFILE=$3;
+
+for file in "$PATH"/*.png
+do
+        output="$(tesseract "$file" stdout --oem 1 --psm 7 -l $LANG)"          
+        echo "$output" >> $OUTFILE                                           
+done 
